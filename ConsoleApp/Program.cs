29e72d54f533b182fdf8a8515a5c3bbf41998be1
fjod <fourjustoneday@yandex.ru>
2020-08-18 09:54:50 +0300

    added common folder between console and webapp

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.IO;
+using System.Linq;
 using System.Threading;
 
 namespace ConsoleApp
@@ -7,13 +9,22 @@ namespace ConsoleApp
     {
         static void Main(string[] args)
         {
+            var fileLocation = System.Reflection.Assembly.GetExecutingAssembly().Location;
+            var directory = Path.GetDirectoryName(fileLocation);
+            
             Console.WriteLine("Hello World from Console app!");
             DateTime finish = DateTime.Now;
-            finish = finish.AddSeconds(10);
+            finish = finish.AddSeconds(1000);
             while (DateTime.Now < finish)
             {
                 Console.WriteLine($"Console App Heartbeat {DateTime.Now.TimeOfDay}");
                 Thread.Sleep(1000);
+                var files = Directory.EnumerateFiles(directory);
+                if (files.Any(f => f.Contains("stop")))
+                {
+                    Console.WriteLine($"Stopping");
+                    break;
+                }
             }
         }
     }
